6e0207879407fb47968d72766e427092bdaddf2a
parkero2 <parkero2@stu.otc.school.nz>
2021-09-27 20:49:53 +1300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const discord = require('discord.js');
+const mockClient = new discord.Client();
+const SpyClient = new discord.Client();
+const fs = require('fs');
+const config = require('./config.json');
+
+SpyClient.on('message', async msg => {
+    let chan = mockClient.guilds.cache.get(config.mock.mockID).channels.cache.find(i => i.name == msg.channel.name);
+    if (chan) {
+        chan.send(`<@${msg.author.id}> : ${msg.content}`);
+    }
+    else {
+        await mockClient.guilds.cache.get(config.mock.mockID).channels.create(msg.channel.name).then(cah => {
+            cah.send(`<@${msg.author.id}> : ${msg.content}`);
+        })
+    }
+});
+
+SpyClient.on('ready', () => {
+    console.log(`Logged in as ${SpyClient.user.username}`);
+});
+
+mockClient.on('ready', () => {
+    console.log(`Logged in as ${mockClient.user.username}`);
+});
+
+mockClient.login(config.mock.mocktoken); //Server you want to forward to
+SpyClient.login(config.spy.spytoken); //The server you want to see
