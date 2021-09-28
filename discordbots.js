e4bff672011275707ed63e6ad0725b2a9d72936e
GalaxyFlight <85672656+GalaxyFlight@users.noreply.github.com>
2021-09-29 12:37:11 +1300

    Updated Version Lol

--- discordbots.js
+++ discordbots.js
@@ -8,22 +8,17 @@ const config = require('./config.json');
 SpyClient.on('message', async msg => {
     let channel = mockClient.guilds.cache.get(config.mock.mockID).channels.cache.find(i => i.name == msg.channel.name);
     let catagory = mockClient.guilds.cache.get(config.mock.mockID).channels.cache.find(i => i.name == msg.channel.parent.name);
-
-    if (catagory) {
-        console.log("Catagory Exists Lol")
-    }
-
     if (channel) {
         // If Channel Exists, Do This:
         if (catagory) {
             // If The Channel AND The Catagory Exist, Do This:
             channel.setParent(catagory)
-            channel.send(`<@${msg.author.id}> : ${msg.content}`)
+            channel.send("<@" + msg.author.id + ">" + ":" + msg.content)
         } else {
             // If the Channel exists but the Catagory does NOT Exist, Do This:
             await mockClient.guilds.cache.get(config.mock.mockID).channels.create(msg.channel.parent.name, { type: 'category' }).then(NewCatagory => {
                 channel.setParent(NewCatagory)
-                channel.send(`<@${msg.author.id}> : ${msg.content}`)
+                channel.send("<@" + msg.author.id + ">" + ":" + msg.content)
             })
         }
     } else {
@@ -32,12 +27,12 @@ SpyClient.on('message', async msg => {
             // If the Channel Does NOT Exist but the Catagory DOES exist, Do This:
             await mockClient.guilds.cache.get(config.mock.mockID).channels.create(msg.channel.name).then(NewChannel => {
                 NewChannel.setParent(catagory)
-                NewChannel.send(`<@${msg.author.id}> : ${msg.content}`);
+                NewChannel.send("<@" + msg.author.id + ">" + ":" + msg.content)
             })
         } else {
             // If the Channel Does NOT Exist AND the Catagory Does NOT Exist, Do This:
             await mockClient.guilds.cache.get(config.mock.mockID).channels.create(msg.channel.name).then(NewChannel => {
-                NewChannel.send(`<@${msg.author.id}> : ${msg.content}`)
+                NewChannel.send("<@" + msg.author.id + ">" + ":" + msg.content)
                 mockClient.guilds.cache.get(config.mock.mockID).channels.create(msg.channel.parent.name, { type: 'category' }).then(NewCatagory => {
                     NewChannel.setParent(NewCatagory);
                 })
