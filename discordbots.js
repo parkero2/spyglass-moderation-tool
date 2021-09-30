7dff6f2f3b4adde67ac73d3cd793c343e5f9183a
parkero2 <parkero2@stu.otc.school.nz>
2021-09-30 14:06:54 +1300

    started exporting data

--- discordbots.js
+++ discordbots.js
@@ -5,6 +5,7 @@ const fs = require('fs');
 const config = require('./config.json');
 
 SpyClient.on('message', async msg => {
+    
     let channel = mockClient.guilds.cache.get(config.mock.mockID).channels.cache.find(i => i.name == msg.channel.name);
     let catagory = mockClient.guilds.cache.get(config.mock.mockID).channels.cache.find(i => i.name == msg.channel.parent.name);
     if (channel) {
@@ -61,6 +62,10 @@ SpyClient.on('ready', () => {
 mockClient.on('ready', () => {
     console.log(`Logged in as ${mockClient.user.username}`);
 });
+
+module.exports = {
+    data : {}
+}
 
 mockClient.login(config.mock.mocktoken); //Server you want to forward to
 SpyClient.login(config.spy.spytoken); //The server you want to see
